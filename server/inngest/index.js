d4cb2af594cfcf01f06acee28ab02dd0d25eb024
muhammad-irtiza-khan <tumhari-new-gmail@gmail.com>
2026-04-05 12:51:41 +0500

    Remove prisma import (not installed)

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,54 +1,38 @@
 import { Inngest } from "inngest";
 
-// Create a client to send and receive events
 export const inngest = new Inngest({ id: "my-app" });
 
-// Create an empty array where we'll export future Inngest functions
-
 const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-from-clerk' },
-    { event: 'clerk/user.created' },
+    { 
+        id: 'sync-user-from-clerk',
+        trigger: { event: 'clerk/user.created' }
+    },
     async ({ event }) => {
         const { data } = event;
-        await prisma.user.create({
-            data: {
-                id: data.id,
-                email: data?.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
-            }
-        });
+        // TODO: Add database logic here when prisma is installed
+        console.log("User created:", data.id);
     }
 );
 
 const syncUserDeletion = inngest.createFunction(
-    { id: 'delete-user-with-clerk' },
-    { event: 'clerk/user.deleted' },
+    { 
+        id: 'delete-user-with-clerk',
+        trigger: { event: 'clerk/user.deleted' }
+    },
     async ({ event }) => {
         const { data } = event;
-        await prisma.user.delete({
-            where: {
-                id: data.id,
-            }
-        });
+        console.log("User deleted:", data.id);
     }
 );
 
-const syncUserUpdation= inngest.createFunction(
-    { id: 'update-user-with-clerk' },
-    { event: 'clerk/user.updated' },
+const syncUserUpdation = inngest.createFunction(
+    { 
+        id: 'update-user-with-clerk',
+        trigger: { event: 'clerk/user.updated' }
+    },
     async ({ event }) => {
         const { data } = event;
-        await prisma.user.update({
-            where: {
-                id: data.id,
-            },
-            data: {
-                email: data?.email_addresses[0]?.email_address,
-                name: data?.first_name + " " + data?.last_name,
-                image: data?.image_url,
-            }
-        });
+        console.log("User updated:", data.id);
     }
 );
 
