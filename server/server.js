0d4f98cdeec004ca87b7ba267385e9b193a7370b
muhammad-irtiza-khan <tumhari-new-gmail@gmail.com>
2026-04-05 20:26:02 +0500

    Restore Inngest express middleware order: json parser before serve

--- server/server.js
+++ server/server.js
@@ -7,10 +7,10 @@ import { inngest, functions } from "./inngest/index.js";
 
 const app = express();
 
-app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use(express.json());
 app.use(cors());
 app.use(clerkMiddleware());
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get('/', (req, res) => {
     res.send('server is live !');
